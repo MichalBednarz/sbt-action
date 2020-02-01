63cf4ca7996ad7024ed921de8039f5a22f02629c
Michał Bednarz <mibed90@gmail.com>
2020-02-01 14:19:19 +0100

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,7 @@
 import scala.io.Source
 
 object Main extends App {
-  val content = getClass.getResourceAsStream("test.txt")
+  val content = getClass.getResourceAsStream("review-comments.json")
   val lines = Source.fromInputStream(content).getLines()
   lines.foreach(println(_))
 }
